879313c74ef965726ee86f7f62ac7df85a3133b4
Gourav Sharma <gouravsharma2806@gmail.com>
2024-07-10 16:51:17 +0530

    modified:   Client/src/components/Login.jsx 	modified:   Client/src/components/Signup.jsx

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -27,12 +27,8 @@ export default function Login() {
                                 </div>
                                 <div class="flex items-center justify-between">
                                     <div class="flex items-start">
-                                        <div class="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                        </div>
-                                        <div class="ml-3 text-sm">
-                                            <label for="remember" class="text-black-500 dark:text-black-300">Remember me</label>
-                                        </div>
+
+
                                     </div>
                                     <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                                 </div>
--- Client/src/components/Signup.jsx
+++ Client/src/components/Signup.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import Login from './Login'
 import { Outlet, Link } from "react-router-dom";
@@ -8,7 +9,7 @@ export default function Signup() {
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-                        Flowbite
+                        Note Application
                     </a>
                     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -29,12 +30,8 @@ export default function Signup() {
                                     <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                                 </div>
                                 <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                                    </div>
+
+
                                 </div>
                                 <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
